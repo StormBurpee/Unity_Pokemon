ab8568b63c24c54443a17cdc6184f79d6504cdd7
Storm Burpee <storm.burpee13@gmail.com>
2016-04-11 23:46:22 +0930

    Episode 2 - Basic Player Movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Direction currentDir;
+    Vector2 input;
+    bool isMoving = false;
+    Vector3 startPos;
+    Vector3 endPos;
+    float t;
+
+    public Sprite northSprite;
+    public Sprite eastSprite;
+    public Sprite southSprite;
+    public Sprite westSprite;
+
+    public float walkSpeed = 3f;
+
+	void Update () { 
+
+        if(!isMoving)
+        {
+            input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            if (Mathf.Abs(input.x) > Mathf.Abs(input.y))
+                input.y = 0;
+            else
+                input.x = 0;
+
+            if(input != Vector2.zero)
+            {
+
+                if(input.x < 0)
+                {
+                    currentDir = Direction.West;
+                }
+                if(input.x > 0)
+                {
+                    currentDir = Direction.East;
+                }
+                if(input.y < 0)
+                {
+                    currentDir = Direction.South;
+                }
+                if (input.y > 0)
+                {
+                    currentDir = Direction.North;
+                }
+
+                switch(currentDir)
+                {
+                    case Direction.North:
+                        gameObject.GetComponent<SpriteRenderer>().sprite = northSprite;
+                        break;
+                    case Direction.East:
+                        gameObject.GetComponent<SpriteRenderer>().sprite = eastSprite;
+                        break;
+                    case Direction.South:
+                        gameObject.GetComponent<SpriteRenderer>().sprite = southSprite;
+                        break;
+                    case Direction.West:
+                        gameObject.GetComponent<SpriteRenderer>().sprite = westSprite;
+                        break;
+                }
+
+                StartCoroutine(Move(transform));
+            }
+
+        }
+
+	}
+
+    public IEnumerator Move(Transform entity)
+    {
+        isMoving = true;
+        startPos = entity.position;
+        t = 0;
+
+        endPos = new Vector3(startPos.x + System.Math.Sign(input.x), startPos.y + System.Math.Sign(input.y), startPos.z);
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime * walkSpeed;
+            entity.position = Vector3.Lerp(startPos, endPos, t);
+            yield return null;
+        }
+
+        isMoving = false;
+        yield return 0;
+    }
+}
+
+enum Direction
+{
+    North,
+    East,
+    South,
+    West
+}
