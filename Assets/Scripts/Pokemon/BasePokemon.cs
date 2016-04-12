37fbb6c221d4cb68cb94e2e8087e7b06c28bbd15
Storm Burpee <storm.burpee13@gmail.com>
2016-04-12 10:51:09 +0930

    Episode 3 - Basic UI/Pokemon

---
+++ Assets/Scripts/Pokemon/BasePokemon.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BasePokemon : MonoBehaviour {
+
+    public string Name;
+    public Sprite image;
+    public BiomeList biomeFound;
+    public PokemonType type;
+    public float baseHP;
+    private float maxHP;
+    public float baseAttack;
+    public float maxAttack;
+    public float baseDef;
+    public float maxDef;
+    public float speed;
+
+    private int level;
+
+	void Start () {
+	
+	}
+	void Update () {
+	
+	}
+}
+
+public enum Rarity
+{
+    VeryCommon,
+    Common,
+    SemiRare,
+    Rare,
+    VeryRare
+}
+
+public enum PokemonType
+{
+    Flying,
+    Ground,
+    Rock,
+    Steel,
+    Fire,
+    Water,
+    Grass,
+    Ice,
+    Electra,
+    Psychic,
+    Dark,
+    Dragon
+}
